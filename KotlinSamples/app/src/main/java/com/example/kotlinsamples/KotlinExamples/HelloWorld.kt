cd5ca8bf83a3ac33c8bd3365de13b6f266e52fc3
MikeStoneColumbia <ms5512@columbia.edu>
2020-05-28 20:15:42 -0700

    added some examples of Kotlin

---
+++ KotlinSamples/app/src/main/java/com/example/kotlinsamples/KotlinExamples/HelloWorld.kt
@@ -0,0 +1,12 @@
+import com.example.kotlinsamples.KotlinExamples.Kotlin
+
+fun main(args:Array<String>){
+
+    println("Hello World") // Standard Hello world
+
+    var KotObj = Kotlin() // Instantiating a Kotlin() object and storing reference KotObj
+    KotObj.name = "Michael Stone" // storing the value "Michael Stone" in the name var
+    println("My name is: ${KotObj.name}") // this is the same as "My name is: " + kotObj.name
+
+
+}
---
+++ KotlinSamples/app/src/main/java/com/example/kotlinsamples/KotlinExamples/Kotlin.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinsamples.KotlinExamples
+
+class Kotlin {
+
+    var name : String = ""
+
+}
